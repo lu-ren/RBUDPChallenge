f16fd89b03b0d73d363e8c42369ab72b543d79cb
Luren Wang <lurenwang@gmail.com>
2016-10-05 17:35:23 -0400

    don't know what to say

--- server.py
+++ server.py
@@ -21,18 +21,22 @@ class UDPServer(object):
         with open(configPath, 'r') as f:
             config = json.load(f)
             for streamConfig in config:
-                with open(streamConfig['binary_path']) as fb:
-                    data = fb.read()
-                    self.streams[streamConfig['id']] = data
+                self.streams[streamConfig['id']] = UDPStream(streamConfig['binary_path'])
 
     def run(self):
         print "Serving is listening to port %d" % self.port
 
         while True:
             data = self.socket.recv(self.bufsz)
+            pdb.set_trace()
             udp = UDPStruct(data)
-            #pdb.set_trace()
             #print(udp)
+
+    def validateCkSum(self, udp):
+        pass
+
+    def validateSeq(self, udp):
+        return self.streams[udp.id].seq == udp.seq
 
 
 class UDPStruct(object):
@@ -40,18 +44,24 @@ class UDPStruct(object):
     def __init__(self, data):
         cksum = data[12:-64]
 
-        self.id = data[:4]
-        self.seq = data[4:8]
-        self.key = data[8:10]
-        self.numcksum = data[10:12]
-        self.header = data[:12]
-        self.cksum = [cksum[x:x + 4] for x in xrange(0, len(cksum), 4)]
+        self.id = binascii.hexlify(data[:4])
+        self.seq = ByteHelper.bytesToUInt(data[4:8])
+        self.key = ByteHelper.bytesToUInt(data[8:10])
+        self.numcksum = ByteHelper.bytesToUInt(data[10:12])
+        self.cksum = [ByteHelper.bytesToUInt(cksum[x:x + 4]) for x in xrange(0, len(cksum), 4)]
         self.sig = data[-64:]
 
     def __repr__(self):
-        return '<id: %s, seq: %d, key: %s, numcksum: %d>' % (binascii.hexlify(self.id), 
-                ByteHelper.bytesToUInt(self.seq), binascii.hexlify(self.key), 
-                ByteHelper.bytesToUInt(self.numcksum))
+        return '<id: %s, seq: %d, key: %s, numcksum: %d>' % (self.id, 
+                self.seq, self.key, self.numcksum)
+
+class UDPStream(object):
+
+    def __init__(self, binary_path):
+        self.seq = 0
+
+        with open(binary_path) as f:
+            self.data = f.read()
 
 class ByteHelper(object):
 
@@ -61,15 +71,7 @@ class ByteHelper(object):
             return struct.unpack('>H', byteStr)[0]
         if len(byteStr) == 4:
             return struct.unpack('>L', byteStr)[0]
-        raise ValueError('Byte string must have length of 2 or 4')
-
-    @staticmethod
-    def bytesToInt(byteStr):
-        if len(byteStr) == 2:
-            return struct.unpack('>h', byteStr)[0]
-        if len(byteStr) == 4:
-            return struct.unpack('>l', byteStr)[0]
-        raise ValueError('Byte string must have length of 2 or 4')
+        raise ValueError('Hex string must have length of 2 or 4')
 
     @staticmethod
     def getCRC32(data, cyclic=None):
